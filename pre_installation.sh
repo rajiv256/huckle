e778058abb305fe9a1d3edbad1deb27044838f50
rajiv <rajivpensidpri@gmail.com>
2018-02-05 00:34:44 +0530

    Added pre-installation bash script

--- pre_installation.sh
+++ pre_installation.sh
@@ -1,1 +1,9 @@
 curl https://sh.rustup.rs -sSf | sh
+# Add $HOME/.cargo/bin to the PATH in .bashrc
+export PATH=$HOME/.cargo/bin:$PATH
+rustup toolchain install nightly-2017-05-04-x86_64-unknown-linux-gnu
+git clone https://github.com/rajiv256/ouros.git
+cd ouros
+rustup override add nightly-2017-04-05
+sudo apt-get install uml-utilities
+sudo apt-get install bridge-utils
